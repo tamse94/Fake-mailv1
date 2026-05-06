b8478afbd99e7f6b8e50f01aee4833fbcbd74b45
tamse94 <kopetraja254@gmail.com>
2026-05-06 07:52:07 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,27 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="id">
       <Head>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet" />
-        {/* Tambahan Link Ikon Google Font */}
+        {/* Google Font: Jost */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
+
+        {/* Material Icons (Tetap kita pertahankan untuk ikon UI) */}
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+
+        {/* Favicon & Web Icons (Ikon di tab browser) */}
+        <link rel="icon" type="image/x-icon" href="/icon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icon.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icon.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icon.png" />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
